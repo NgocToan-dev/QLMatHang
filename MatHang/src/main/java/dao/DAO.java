9589ffba0fa801c6fd38265c3613518f6441a936
Luckfier <nguyenvietnamhg@gmail.com>
2022-04-17 18:00:43 +0700

    them jstl

--- MatHang/src/main/java/dao/DAO.java
+++ MatHang/src/main/java/dao/DAO.java
@@ -9,11 +9,11 @@ public class DAO {
     public DAO(){
         if(con == null){
             String dbUrl =
-                    "jdbc:mysql://azuinst.tk:3306/qlmathang?autoReconnect=true&useSSL=false&clientInteractive=true";
+                    "jdbc:mysql://localhost:3306/qlmathang?autoReconnect=true&useSSL=false&clientInteractive=true";
             String dbClass = "com.mysql.cj.jdbc.Driver";
             try {
                 Class.forName(dbClass);
-                con = DriverManager.getConnection (dbUrl, "admin", "admin@1234");
+                con = DriverManager.getConnection (dbUrl, "root", "0948187691");
             }catch(Exception e) {
                 e.printStackTrace();
             }
