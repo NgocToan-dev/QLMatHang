dc041f9f61395f0b5f6c65931718d8606b56ab80
T-Q-H <tranquanghuy05072000@gmail.com>
2022-04-19 23:58:52 +0700

    login,listMh,delete

---
+++ MatHang/src/main/java/com/example/mathang/LogoutServlet.java
@@ -0,0 +1,19 @@
+package com.example.mathang;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", value = "/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        HttpSession session = request.getSession();
+        session.removeAttribute("account");
+        response.sendRedirect("index.jsp");
+
+    }
+}
